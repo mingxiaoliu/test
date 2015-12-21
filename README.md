c07ca4cd5bca170db86075303d056baeafbdec92
mingxiaoliu <mingjiuqiang@163.com>
2015-12-21 20:55:34 +0800

    read me again

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ test
 This is the first try of github
 I am back about one year later
 I have add some new test for vim
+I have add some new test for hello
+I have add some new test for git rebase
--- hello/hello.c
+++ hello/hello.c
@@ -5,5 +5,10 @@ int main(void)
     int i = 10;
     int j = 20;
     printf("Hello GitHub %d, %d\n", i, j);
+    printf("Hello GitHub %d, %d new\n", i, j);
+
+	if (i == 0 ) {
+		printf("%s", "This is first try of ycm \n");
+	}
     return 0;
 }
